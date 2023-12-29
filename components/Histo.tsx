66fe417d8833e4f39a352e6a5ce41a31e739af77
Nnamdi Okeke <onnamdialex@gmail.com>
2023-12-29 18:08:50 +0100

    :construction: Wip, fetched the data. now on to the course to plot the histogram

--- components/Histo.tsx
+++ components/Histo.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+//to fetch the numbers from the API endpoint
+async function fetchNumbers(): Promise<number[]> {
+  const response = await fetch(
+    "https://www.random.org/integers/?num=200&min=1&max=10&col=1&base=10&format=plain&rnd=new"
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed");
+  }
+
+  const text = await response.text();
+  const numbers = text
+    .trim()
+    .split("\n")
+    .map((numStr) => parseInt(numStr.trim(), 10));
+
+  return numbers;
+}
 
 const Histo = () => {
+  //
+
   return <div>Histo</div>;
 };
 

f6b38f59686aff29dc8838047bd18e334f620b0f
Nnamdi Okeke <onnamdialex@gmail.com>
2023-12-29 20:02:13 +0100

    :zap: Improve performance and implementation of histogram logic done

--- components/Histo.tsx
+++ components/Histo.tsx
@@ -1,3 +1,5 @@
+"use client"; // This is a client component 👈🏽
+
 import React, { useState, useEffect } from "react";
 
 //to fetch the numbers from the API endpoint
@@ -20,9 +22,108 @@ async function fetchNumbers(): Promise<number[]> {
 }
 
 const Histo = () => {
-  //
+  //state for the histogram data
+  const [data, setData] = useState<number[]>([]);
 
-  return <div>Histo</div>;
+  useEffect(() => {
+    // Fetch data when component mounts
+    async function fetchData() {
+      try {
+        const numbers = await fetchNumbers();
+        setData(numbers);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  //another effect hook for the histogram Logic
+  useEffect(() => {
+    //function to draw the histogram
+    function drawHistogram(data: number[]) {
+      //to get the id of the canvas html element hosting the graph
+      const canvas = document.getElementById(
+        "histogramCanvas"
+      ) as HTMLCanvasElement;
+      const ctx = canvas.getContext("2d");
+
+      if (!ctx) {
+        console.error("canvas context unavailable");
+        return;
+      }
+
+      //drawing a clean slate
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      const maxValue = Math.max(...data);
+      const barWidth = (canvas.width - 60) / data.length; // Adjusted for better spacing
+      const padding = 40;
+      const barSpacing = 20;
+
+      // Draw x-axis
+      ctx.beginPath();
+      ctx.moveTo(padding, canvas.height - padding);
+      ctx.lineTo(canvas.width + padding, canvas.height - padding);
+      ctx.stroke();
+
+      // Draw y-axis
+      ctx.beginPath();
+      ctx.moveTo(padding, canvas.height - padding);
+      ctx.lineTo(padding, padding);
+      ctx.stroke();
+
+      // Draw x-axis labels
+      ctx.fillStyle = "black";
+      ctx.textAlign = "center";
+      ctx.textBaseline = "top";
+      data.forEach((_, index) => {
+        const label = index + 1;
+        const x = padding + index * (barWidth + barSpacing) + barWidth / 2;
+        ctx.fillText(label.toString(), x, canvas.height - padding + 10);
+      });
+
+      // Draw y-axis labels
+      ctx.textAlign = "right";
+      ctx.textBaseline = "middle";
+      const maxLabel = Math.ceil(maxValue / 10) * 10;
+      for (let i = 0; i <= 5; i++) {
+        const label = (maxLabel / 5) * i;
+        const y =
+          canvas.height - padding - (canvas.height - 2 * padding) * (i / 5);
+        ctx.fillText(label.toString(), padding - 10, y);
+      }
+
+      // Draw bars
+      data.forEach((value, index) => {
+        const barHeight = (value / maxValue) * (canvas.height - 2 * padding);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(
+          padding + index * (barWidth + barSpacing),
+          canvas.height - padding - barHeight,
+          barWidth,
+          barHeight
+        );
+      });
+    }
+    drawHistogram(data);
+  }, [data]);
+
+  return (
+    <div>
+      <h2
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Histogram
+      </h2>
+      <canvas id="histogramCanvas" width={400} height={300}></canvas>
+    </div>
+  );
 };
 
 export default Histo;
